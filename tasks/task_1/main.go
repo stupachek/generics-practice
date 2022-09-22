e77bf34e0a8c22723c053ad9af8434247f4e640b
stupachek <alona.stupak@gmail.com>
2022-09-22 19:01:21 +0300

    task1

--- tasks/task_1/main.go
+++ tasks/task_1/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type List[T any] struct {
 	elems []T
 }
@@ -12,11 +14,33 @@ func (l *List[T]) Update(i int, t T) {
 	l.elems[i] = t
 }
 
-func (l *List[T]) Pop() {
+func (l *List[T]) Pop() T {
+	last := l.elems[len(l.elems)-1]
+	l.elems = l.elems[:len(l.elems)]
+	return last
+}
 
+func (l *List[T]) Delete(i int) {
+	l.elems = append(l.elems[:i], l.elems[i+1:]...)
 }
 
 // Task: create chaining methods Add, Update, Pop, Delete
 func main() {
+	list := List[string]{
+		elems: []string{"qwe", "rty", "tyu"},
+	}
+	fmt.Println(list.elems)
+
+	list.Add("dfgdfgdfgfd")
+	fmt.Println("\n", list.elems)
+
+	list.Delete(2)
+	fmt.Println("\n", list.elems)
+
+	l := list.Pop()
+	fmt.Println("\n", l, list.elems)
+
+	list.Update(0, "123")
+	fmt.Println("\n", list.elems)
 
 }
